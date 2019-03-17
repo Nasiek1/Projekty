a7dfe7aa97d6d2638b177aee949c7a092718fb39
olenk <olenk@DESKTOP-2MSTLJ8>
2019-03-17 12:32:13 +0100

    tokeny

--- Projekt/project/project/ListONP.cpp
+++ Projekt/project/project/ListONP.cpp
@@ -3,7 +3,7 @@
 
 void ListONP::insertElement(std::unique_ptr<TerminalExpression> terminalExp)
 {
-	if()
+	
 
 }
 void ListONP::deleteElement(std::unique_ptr<TerminalExpression> terminalExp)
@@ -13,7 +13,7 @@ void ListONP::deleteElement(std::unique_ptr<TerminalExpression> terminalExp)
 }
 std::unique_ptr<TerminalExpression> ListONP::getElement()
 {
-	std::listd::unique_ptr<TerminalExpression>>::iterator findIter = std::find(terminalExpList.begin(), terminalExpList.end(), terminalExp);
+	std::list<std::unique_ptr<TerminalExpression>>::iterator findIter = std::find(terminalExpList.begin(), terminalExpList.end(), terminalExp);
 	//if (findIter != terminalExpList.end())
 		return findIter.operator*;
 }
